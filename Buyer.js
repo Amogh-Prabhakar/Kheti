706dcab43953f1e2023f9fe512876383a67672eb
Swastik Mukherjee <82890981+aarush202070@users.noreply.github.com>
2024-11-17 13:51:29 +0530

    Add files via upload

---
+++ Buyer.js
@@ -0,0 +1,13 @@
+
+function BP()
+{
+    window.location = "kheti buyers.html";
+}
+
+function BI(){
+    window.location = "Buyers.html";
+}
+
+function Title(){
+    window.location = "index.html";
+}
---
+++ main.js
@@ -0,0 +1,28 @@
+
+ 
+
+function HI() {
+    window.location = "Home.html";
+}
+function W() {
+    window.location = "crop return.html";
+}
+function SP() {
+    window.location = "supplies.html";
+}
+function BY() {
+    window.location = "buy.html";
+}
+function Sellers(){
+    window.location = "supplies.html";
+}
+function CGA()
+{
+    window.location ="crop growth advisor.html";
+}
+function IN(){
+    window.location ="Buyers.html";
+}
+function BU(){
+    window.location ="cart.html";
+}
